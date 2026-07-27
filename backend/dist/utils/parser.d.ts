1502fb693de71f9f689e807568e6ec23a7d61062
Joel Miller <millerjoel7597@gmail.com>
2026-07-27 09:11:46 +0100

    chore: add/update backend/dist/utils/parser.d.ts

---
+++ backend/dist/utils/parser.d.ts
@@ -0,0 +1,7 @@
+import { ComponentProp } from './db.js';
+/**
+ * Parses React component code (TSX/JSX) to automatically extract properties,
+ * types, default values, and description JSDoc comments.
+ */
+export declare function parsePropsFromCode(code: string): ComponentProp[];
+//# sourceMappingURL=parser.d.ts.map
